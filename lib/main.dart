5359ebfb1e22dafe6b1b51114099ad5b82668565
singgihwiro <singgihwiro23@gmail.com>
2025-02-26 20:57:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello World",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: "Flutter Hello World Page"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: const Center(
+        child: Text('Hello World', style: TextStyle(fontSize: 20)),
+      ),
+    );
+  }
+}
